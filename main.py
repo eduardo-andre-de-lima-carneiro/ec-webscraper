ceea6201dee1fe16128ee5f93fff1cd855c624e2
Eduardo Andre de Lima Carneiro <eandre.carneiro@gmail.com>
2025-04-10 15:47:08 -0400

    Adding extract_data function and update requirements.txt

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
+import logging
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 from scraper.fetcher import WebFetcher
 from scraper.parser import HTMLDataParser
 from scraper.exporter import DataExporter
 from bs4 import BeautifulSoup
 
 def main():
-    url = "https://example.com"
-    base_url = "https://example.com"
+    load_dotenv()
+    url = os.getenv("URL")
     html = WebFetcher.fetch(url)
 
     if html:
-        soup = BeautifulSoup(html, "html.parser")
-
-        models = HTMLDataParser.extract_text(soup, "h2", {"class": "headline bri-txt ff-b title-three"})
-        links = HTMLDataParser.extract_attribute(soup, "a", "href", {"class": "cta one btn-icon"}, base_url)
-        anchors = HTMLDataParser.extract_anchor_ids(links)
-
-        data = [{"Model": m, "Link": l, "Anchor": a} for m, l, a in zip(models, links, anchors)]
+        data = extract_data(html)
 
         DataExporter.to_csv(data, "output.csv")
         DataExporter.to_json(data, "output.json")
         print("✅ Export done.")
     else:
         print("❌ Failed to retrieve HTML.")
+
+def extract_data(html):
+        load_dotenv()
+        block_class = os.getenv("BLOCK_CLASS")
+        name_class = os.getenv("NAME_CLASS")
+        info_class = os.getenv("INFO_CLASS")
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(__name__)
+
+        soup = BeautifulSoup(html, "html.parser")
+        data = []
+        data_blocks = soup.find_all("div", class_=block_class)
+
+        print(data_blocks)
+
+        for block in data_blocks:
+            try:
+                #Name
+                name = block.find("a", class_=name_class).get_text(strip=True).replace("®", "")
+                info_container = block.find_next("div", class_=info_class)
+
+                # Price
+                # price_span = info_container.select_one("span[data-pricing-template]")
+                # price = price_span.get("data-pricing-template", "").replace("{price}", "").strip()
+
+                # Badge
+                badge_label = info_container.find("span", class_="badges-label")
+                badge_text = badge_label.get_text(strip=True) if badge_label else "N/A"
+
+                data.append({
+                    "name": name,
+                    "badge": badge_text
+                })
+            except Exception as e:
+                logger.warning(f"Could not parse a data block: {e}")
+                continue
+
+        return data
 
 if __name__ == "__main__":
     main()
